87a11b0999d17c4721e1889b41c5329365edf40a
Poovarasan <spoovarasan482@gmail.com>
2026-09-22 21:51:51 +0530

    Initial Python factorial application

---
+++ test_factorial.py
@@ -0,0 +1,16 @@
+import pytest
+
+from factorial import factorial
+
+def test_factorial_of_zero():
+    assert factorial(0) == 1
+
+def test_factorial_of_five():
+    assert factorial(5) == 120
+
+def test_factorial_of_ten():
+    assert factorial(10) == 3628800
+
+def test_negative_number():
+    with pytest.raises(ValueError):
+        factorial(-1)
