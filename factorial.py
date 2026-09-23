7771be3bc0e74c4f96843a88d8424b75b77fc615
Poovarasan <spoovarasan482@gmail.com>
2026-09-23 09:18:11 +0530

    Update factorial implementation

--- factorial.py
+++ factorial.py
@@ -2,6 +2,6 @@ def factorial(n):
     if n < 0:
         raise ValueError("Negative number")
     result = 1
-    for i in range(2, n + 1):
+    for i in range(2, n+1,1):
         result *= i
     return result
